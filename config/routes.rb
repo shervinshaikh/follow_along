4508242e390f98e2f0170bb25e29be151b37e0a2
Shervin Shaikh <shervinater@gmail.com>
2012-11-03 15:41:32 -0700

    removed the slideshow.html file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 FollowAlong::Application.routes.draw do
 
   resources :presentations
-  
-  # Needs to be in own controller - TODO
-  match '/slideshow',  to: 'home#slideshow'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
